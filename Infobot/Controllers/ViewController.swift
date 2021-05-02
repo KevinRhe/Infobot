e9624c79ca2deffb1fcb1278742434de5ebcdecf
Kevin Rheynaldi <kevinrheynaldi298@gmail.com>
2021-05-02 12:41:25 +0700

    Initial Commit

---
+++ Infobot/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Infobot
+//
+//  Created by Kevin Rheynaldi on 02/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
